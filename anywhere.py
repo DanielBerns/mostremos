a8471e3ece02035eded1b03673d03ca569cb6e7e
DanielBerns <daniel.w.berns@gmail.com>
2026-04-22 12:26:08 -0300

    fixing anywhere 1

--- anywhere.py
+++ anywhere.py
@@ -5,8 +5,10 @@ current_dir = Path(__file__).resolve().parent
 packages_dir = current_dir.parent
 
 if str(packages_dir) not in sys.path:
-sys.path.insert(0, str(packages_dir))
+    sys.path.insert(0, str(packages_dir))
 
-from .api import create_app
+print(sys.path)
+
+from api import create_app
 
 app = create_app()
