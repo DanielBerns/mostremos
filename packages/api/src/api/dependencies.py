fdeddfcf604794bad9a6370f39685ef7317f08de
DanielBerns <daniel.w.berns@gmail.com>
2026-04-24 18:29:17 -0300

    complete with user register 4

--- packages/api/src/api/dependencies.py
+++ packages/api/src/api/dependencies.py
@@ -1,13 +1,13 @@
 from infra.database.engine import SessionLocal
-from infra.database.adapters import SqlAlchemySubmissionRepository
+from infra.database.adapters import SqlAlchemySubmissionRepository, SqlAlchemyUserRepository
 from infra.storage.adapters import LocalDiskStorageAdapter
-from infra.storage.adapters import SqlAlchemyUserRepository
+
 
 # Initialize secondary adapters
 _session = SessionLocal()
 submission_repo = SqlAlchemySubmissionRepository(_session)
-storage_adapter = LocalDiskStorageAdapter()
 user_repo = SqlAlchemyUserRepository(_session)
+storage_adapter = LocalDiskStorageAdapter()
 
 def get_submission_repo():
     return submission_repo
